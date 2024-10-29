36ea50d01c8f3f415cf67338aac5e38c16af464c
berkay <erdogan.berkay.uckuyulu@hotmail.com>
2024-10-29 17:42:48 +0300

    genel mimari

---
+++ src/Order.API/Program.cs
@@ -0,0 +1,20 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapPost("/create", () =>
+{
+
+});
+
+app.Run();
+
---
+++ src/OrderInboxTableSaverService/Program.cs
@@ -0,0 +1,8 @@
+﻿using OrderInboxTableSaverService;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddHostedService<Worker>();
+
+var host = builder.Build();
+host.Run();
+
